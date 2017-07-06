35f03666499bd58c64cf1cd5a4f3d8e2a8d5d1c9
FengYuHe <heyufeng8080@gmail.com>
2017-07-06 09:52:45 +0800

    init code

---
+++ index.js
@@ -0,0 +1,21 @@
+/**
+ * Created by heyufeng on 2017/7/6.
+ */
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.post('/kdniao', function(req, res) {
+    console.log(req.body);
+    res.send(true);
+});
+
+app.listen(4142, ()=> {
+    console.log(`Listen port to 4142`);
+});
